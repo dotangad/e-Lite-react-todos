58ec86dc203c70a0e1df8d6b6cb5acf39968e88d
Angad Singh <mail@angad.dev>
2021-05-31 19:43:48 +0530

    feat: Write app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
 
 function App() {
+  const [todos, setTodos] = useState([
+    "Clean the kitchen",
+    "Get groceries",
+    "Cook dinner"
+  ])
+  const [inputState, setInputState] = useState("")
+
+  const handleAdd = e => {
+    e.preventDefault()
+
+    setTodos([
+      ...todos,
+      inputState
+    ])
+
+    setInputState("")
+  }
+
+  const handleDelete = (i) => (e) => {
+    setTodos(todos_ => 
+      todos_.filter((_, k) => i !== k)
+    )
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TODO list</h1>
+      <div>
+        <input
+          type="text"
+          placeholder="New todo"
+          value={inputState}
+          onChange={e => setInputState(e.target.value)}
+        />
+        <button onClick={handleAdd}>Add</button>
+      </div>
+      <ul>
+        {todos.map((todo, i) => 
+          <li key={i} onClick={handleDelete(i)}>{todo}</li>
+        )}
+      </ul>
     </div>
   );
 }
